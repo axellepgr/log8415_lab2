a0b2a998d62a9a6431561f88c4b415280cbd9fdc
Axelle <axelle.pagnier@gmal.com>
2022-10-25 17:18:02 -0400

    get vpc and subnet ids automatically + add menu

--- src/set_up.py
+++ src/set_up.py
@@ -1,3 +1,4 @@
+from re import sub
 import boto3
 import time
 
@@ -12,8 +13,27 @@ AMI_ID = "ami-08c40ec9ead489470"
 
 ec2_client = boto3.client("ec2", region_name=AWS_REGION)
 ec2_resource = boto3.resource('ec2', region_name=AWS_REGION)
-vpcID = 'vpc-087ef03df262abb66'
-subnet_id = 'subnet-066c5c280a820dc2d'
+
+def get_vpc_id_and_subnet():
+    """
+    This function returns the id of the default vpc and the first subnet.
+    Returns vpc_id, subnet_id.
+    """
+    response = ec2_client.describe_vpcs()
+    vpc_id = response['Vpcs'][0]['VpcId']
+    response = ec2_client.describe_subnets(
+        Filters=[
+                {
+                    'Name': 'vpc-id',
+                    'Values': [
+                        vpc_id,
+                    ],
+                },
+            ],
+        )
+    subnet_id = response['Subnets'][0]['SubnetId']
+    return vpc_id, subnet_id
+    
 def create_sg(vpcID):
     """
     This function creates a new security group for the VPC.
@@ -70,10 +90,10 @@ def create_ec2_instances(nbr, type, sg_id, subnet_id):
 
 print("\n############### SETTING UP THE SYSTEM ###############\n")
 
-print("Security group created!\n")
-
-print("Creating the EC2 instances...")
+vpcID, subnet_id = get_vpc_id_and_subnet()
 sg_id = create_sg(vpcID)
+print("Security group created!\n")
+print("Creating the EC2 instances...")
 create_ec2_instances(1, 0, sg_id, subnet_id)
 print("EC2 instances created!\n")
 
