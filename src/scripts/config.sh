45894dda3ca6a207ae379312b099725c1c7544e6
Axelle <axelle.pagnier@gmal.com>
2022-10-27 14:52:24 -0400

    fix config.sh and run_hadoop.sh

--- src/scripts/config.sh
+++ src/scripts/config.sh
@@ -1,13 +1,14 @@
+#! /bin/bash
+
 sed -i 's/# export JAVA_HOME=/export JAVA_HOME=\/usr\/lib\/jvm\/java-11-openjdk-amd64\//' /usr/local/hadoop/etc/hadoop/hadoop-env.sh
-cd /usr/local/hadoop/sbin
-# install Java, maybe no need ?
+# install Java
 sudo apt-get update
 yes | sudo apt-get install openjdk-11-jdk
 # Solving key problems
 cd ~/.ssh
 echo | ssh-keygen -P ''
 cat id_rsa.pub >> authorized_keys
-# start 
+# start
 cd /usr/local/hadoop/sbin
 ./start-dfs.sh
 cd ~/.ssh
@@ -15,6 +16,14 @@ export HADOOP_INSTALL=/usr/local/hadoop && export PATH=$HADOOP_INSTALL/bin:$PATH
 export PATH=$PATH:/usr/local/hadoop/bin/
 
 mv ~/run_hadoop.sh /usr/local/hadoop/sbin
-mv ~/time.sh /usr/local/hadoop/sbin
 mv ~/WordCount.java /usr/local/hadoop/sbin
-(cd /usr/local/hadoop/sbin/ ; sh run_hadoop.sh)
+read -p "All good ? (y/n): " y
+if [[ $y == 'y' ]]
+then
+        (cd /usr/local/hadoop/sbin/ ; sh run_hadoop.sh)
+else
+        echo "try running cd /usr/local/hadoop/sbin/ && sh run_hadoop.sh "
+        exit 0
+fi
+echo -e "\nIf that failed, try running : \n"
+echo -e "$              cd /usr/local/hadoop/sbin/ && sh run_hadoop.sh \n"
--- src/scripts/run_hadoop.sh
+++ src/scripts/run_hadoop.sh
@@ -9,5 +9,6 @@ echo "\n Files available : \n"
 hadoop fs -ls input/
 echo "\nPlease select file : "
 read file
+hdfs dfs -rm output
 hadoop jar wc.jar WordCount input/$file output
 hadoop fs -cat output/part-r-00000
