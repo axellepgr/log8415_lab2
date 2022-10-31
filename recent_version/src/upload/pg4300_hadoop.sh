1b98826910b11d2fe376bd61282e02c8e0bf33d3
Mohammed Ramzi Bouthiba <chlef-algerie@hotmail.fr>
2022-10-31 14:59:02 -0400

    fixed hadoop job error

--- recent_version/src/upload/pg4300_hadoop.sh
+++ recent_version/src/upload/pg4300_hadoop.sh
@@ -5,7 +5,7 @@ curl -O https://www.gutenberg.org/cache/epub/4300/pg4300.txt
 hdfs dfs -copyFromLocal ~/pg4300.txt input
 hadoop com.sun.tools.javac.Main WordCount.java
 jar cf wcpg4300.jar WordCount*.class
-{ time hadoop jar wc.jar WordCount input output ; } 2>> pg4300_hadoop_output.txt
+{ time hadoop jar wcpg4300.jar WordCount input output ; } 2>> pg4300_hadoop_output.txt
 tail -n 3 pg4300_hadoop_output.txt >> pg4300_hadoop_time.txt
 rm pg4300_hadoop_output.txt
 rm 'WordCount$TokenizerMapper.class'
